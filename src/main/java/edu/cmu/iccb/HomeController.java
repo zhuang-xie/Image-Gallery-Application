d4004ce5df63787c22c68c29917c013ef129f991
zhuang-xie <12051381@life.hkbu.edu.hk>
2016-09-19 22:30:32 -0400

    Revert "Added an oauth cookie"

--- src/main/java/edu/cmu/iccb/HomeController.java
+++ src/main/java/edu/cmu/iccb/HomeController.java
@@ -1,14 +1,11 @@
 package edu.cmu.iccb;
 
-import java.util.List;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,54 +16,45 @@ import edu.cmu.iccb.services.ImageService;
 
 @Controller
 public class HomeController {
-
-	private ImageService imageService;
-
-	@Autowired
+	
+    private ImageService imageService;
+    
+    @Autowired
 	public void setImageService(ImageService imageService) {
 		this.imageService = imageService;
 	}
 
-	public ImageService getImageService() {
+    public ImageService getImageService() {
 		return imageService;
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "/images")
-	public String provideUploadInfo(Model model, RedirectAttributes redirectAttributes,
-			@CookieValue(value = "oauth", defaultValue = "invalid") String oauthCookie) {
-		if (oauthCookie.equals("invalid"))
-			return "redirect:/";
+    @RequestMapping(method = RequestMethod.GET, value = "/images")
+    public String provideUploadInfo(Model model, RedirectAttributes redirectAttributes) {
 
-		List<String> imageIds = imageService.getUploadedImages();
-		model.addAttribute("files", imageIds);
-		return "uploadForm";
-	}
+        List<String> imageIds = imageService.getUploadedImages();        
+        model.addAttribute("files", imageIds);     
+        return "uploadForm";
+    }
 
-	@RequestMapping(method = RequestMethod.POST, value = "/images")
-	public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes,
-			@CookieValue(value = "oauth", defaultValue = "invalid") String oauthCookie) {
-		if (oauthCookie.equals("invalid"))
-			return "redirect:/";
+    @RequestMapping(method = RequestMethod.POST, value = "/images")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
 
-		String name = file.getOriginalFilename();
+        String name = file.getOriginalFilename();
 
-		try {
-			imageService.saveImage(name, file.getInputStream());
-		} catch (Exception e) {
-			redirectAttributes.addFlashAttribute("message", name + " failed to upload");
-		}
+        try {        	
+        	imageService.saveImage(name, file.getInputStream());           
+        }
+        catch (Exception e) {
+            redirectAttributes.addFlashAttribute("message", name + " failed to upload");
+        }
 
-		return "redirect:/images";
-	}
+        return "redirect:/images";
+    }
 
-	@RequestMapping(method = RequestMethod.GET, value = "/")
-	public String loginForm(Model model, RedirectAttributes redirectAttributes) {
-		return "login";
-	}
-
-	@RequestMapping(method = RequestMethod.GET, value = "/oauth")
-	public String postOauth(RedirectAttributes redirectAttributes, HttpServletResponse response) {
-		response.addCookie(new Cookie("oauth", "valid"));
-		return "redirect:/images";
-	}
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String loginForm(Model model, RedirectAttributes redirectAttributes) {   
+        return "login";
+    }
 }
